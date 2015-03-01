e507c8c6c2d38c34a29f13e3a6711b60b6217ef0
Ing. Jonathan Olier <djom202@gmail.com>
2015-03-01 01:26:45 -0500

    Load Images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,111 @@
 $(function(){
-	$game.support();
+	setTimeout(function() {
+		$('#initGame').click();
+	}, 1000);
 });
 
 window.$game = {
 	init: function(canvas){
 		this.log('Init Canvas');
-		window.$lienzo 	= canvas;
 		window.$imgs 	= new imagenes();
-		this.log(canvas);
-		$lienzo.fillStyle = "#fff";
+		window.$lienzo 	= canvas;
+		window.$play 	= false;
+		window.$tap 	= new tapPlacard(35, 75, 10, 10, "tap");
 
-		this.createImages();
+		document.onkeydown = this.onkeydown;
+        document.onkeyup = this.onkeyup;
 	},
-	support: function(){
-		var canvas = document.getElementById('lienzo');
+	support: function(id_canvas){
+		var canvas = document.getElementById(id_canvas);
 		if (canvas.getContext){
 			this.log('Support Canvas');
 			this.init(canvas.getContext('2d'));
+			return true;
 		} else {
-			this.log('No support Canvas');
+			this.log('Your browser does not support the HTML5 canvas tag');
+			return false;
+		}
+	},
+	initGame: function(id_canvas){
+		if(this.support(id_canvas)){
+			this.createImages();
 		}
 	},
 	createImages: function(){
 		this.log('Create Images');
-		$imgs.add("fondo","./assets/imgs/bg2.png");
-		this.log($imgs.get("fondo"));
-		// while (true) {
-			$lienzo.drawImage($imgs.get("fondo"),10, 10, 100, 100);
+		this.log('Bg');
+		$imgs.add("bg", "http://test.altiviaot.com/game/assets/imgs/bg2.png");
+
+		this.log('Bird');
+		$imgs.add("bird", "http://test.altiviaot.com/game/assets/imgs/bird.png");
+
+		this.log('Getready');
+		$imgs.add("getready", "http://test.altiviaot.com/game/assets/imgs/getready.png");
+
+		this.log('Pause');
+		$imgs.add("pause", "http://test.altiviaot.com/game/assets/imgs/pause.png");
+
+		this.log('Tap');
+		$imgs.add("tap", "http://test.altiviaot.com/game/assets/imgs/tap.png");
+
+		this.loadImages();
+	},
+	loadImages: function(){
+		// if(!$imgs.isLoaded()){
+			this.clear();
+
+			this.log('Load Images');
+			this.log('Bg');
+			$lienzo.drawImage($imgs.get("bg"), 0, 0, 600, 150);
+
+			this.log('Bird');
+			$lienzo.drawImage($imgs.get("bird"), 30, 50, 20, 20);
+
+			this.log('Getready');
+			$lienzo.drawImage($imgs.get("getready"), 95, 15, 120, 20);
+
+			this.log('Pause');
+			$lienzo.drawImage($imgs.get("pause"), 285, 5, 10, 10);
+
+			this.log('Tap');
+			$tap.brush($lienzo);
+
+			if($imgs.isLoaded()){
+				this.log('Loaded');
+				// this.gameLoop();
+			} else {
+				this.log('No Loaded');
+			}	
 		// }
+	},
+	gameLoop: function () {
+		this.getRequestAnimationFrame(this.gameLoop);
+		this.loadImages();
+	},
+	getRequestAnimationFrame: function(callback){
+       	window.setTimeout(callback, 1000 / 60);
+	},
+	onkeydown:  function(e){
+		if(e.keyCode == 32) { // Espace
+            // HERE
+        } else if (e.keyCode == 27) { // ESC
+            // HERE
+        }
+	},
+	onkeyup:  function(e){
+		if(e.keyCode == 32) {
+            // HERE
+        }
+	},
+	waitActions:  function(){
+
+	},
+	startGame: function(){
+
+	},
+	clear: function(){		
+		this.log('Clear Images');
+		// $lienzo.clearRect(0, 0, $lienzo.width, $lienzo.height);
 	},
 	log: function(value){
 		console.log(value);
--- assets/js/objs.js
+++ assets/js/objs.js
@@ -1,16 +1,16 @@
 // Trato de Imagenes
 function imagenes () {
     var img = [];
-    
+
     this.add = function (imagen, directorio) {
-        img[imagen] = new Image();
+        img[imagen] 	= new Image();
         img[imagen].src = directorio;
     }
-    
+
     this.get = function (imagen) {
         return img[imagen];
     }
-    
+
     this.isLoaded = function () {
         var carga = true;
         for (var k in img) {
@@ -18,7 +18,31 @@ function imagenes () {
                 carga = (!carga)? false : img[k].complete;
             }
         }
-        
+
         return carga;
+    }
+}
+
+function tapPlacard(_x, _y, _w, _h, _image){
+    var image 	= _image;
+    var inicio 	= 1;
+    var escalar = 0.03;
+
+    this.x 		= _x;
+    this.y 		= _y;
+    this.width 	= _w;
+    this.height = _h;
+
+    this.brush = function (lienzo) {
+        lienzo.save();
+        lienzo.translate(this.x, this.y);
+        lienzo.scale(inicio/3, inicio/3);
+        lienzo.drawImage($imgs.get(image), -this.width/2, -this.height/2);
+        lienzo.restore();
+
+        inicio += escalar;
+        if(inicio > 1 || inicio < 0.5) {
+            escalar = -escalar;
+        }
     }
 }
